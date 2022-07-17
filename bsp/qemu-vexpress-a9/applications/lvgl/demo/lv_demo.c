74ba492bd0409d9947886b3fa6100eee0b0f0ed8
Varanda <m@varanda.ca>
2022-07-17 10:23:22 -0400

    increase stack to attempt to solve crash. still crashing.

--- bsp/qemu-vexpress-a9/applications/lvgl/demo/lv_demo.c
+++ bsp/qemu-vexpress-a9/applications/lvgl/demo/lv_demo.c
@@ -12,6 +12,11 @@
 void lv_user_gui_init(void)
 {
     /* display demo; you may replace with your LVGL application at here */
+#ifdef PKG_USING_GUITAR_PEDALS
+    extern void ui_init(void);
+    ui_init();
+#else
     extern void lv_demo_music(void);
     lv_demo_music();
+#endif
 }
